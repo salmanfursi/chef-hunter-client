20bd02babd761a104e4fb220e985f36770489ae3
unknown <salmanfursipro@gmail.com>
2023-05-04 21:04:52 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+## Some README.md Websites
+
+ - [Awesome Readme Templates](https://awesomeopensource.com/project/elangosundar/awesome-README-templates)
+ - [Awesome README](https://github.com/matiassingers/awesome-readme)
+ - [How to write a Good readme](https://bulldogjob.com/news/449-how-to-write-a-good-readme-for-your-github-project)
+
+ - [How to write a Good readme](https://bulldogjob.com/news/449-how-to-write-a-good-readme-for-your-github-project)
+
+ - [How to write a Good readme](https://bulldogjob.com/news/449-how-to-write-a-good-readme-for-your-github-project)
+
+
+
+## Live Link
+
+[Link](https://choosealicense.com/licenses/mit/) : https://chef-recipe-server-lake.vercel.app/recipe
+
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,51 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Login from "../components/Login/Login";
+import Register from "../components/Register/Register";
+import Blog from "../components/Blog/Blog";
+import Home from "../components/Home/Home";
+import RecipeDetails from "../components/RecipeDetails/RecipeDetails";
+import PrivetRoute from './PrivetRoute';
+import ErrorePage from './../components/Error/ErrorePage';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorePage></ErrorePage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "/recipeDetails/:id",
+        element: (
+          <PrivetRoute>
+            <RecipeDetails></RecipeDetails>
+          </PrivetRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(`https://chef-recipe-server-lake.vercel.app/recipe/${params.id}`),
+      },
+      
+    ],
+  },
+]);
+
+export default router;
